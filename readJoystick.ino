7e3d48785de312e1ab77c8182c0d08c357155e4a
Kuba Vejražka <49091814+KubaVejrazka@users.noreply.github.com>
2022-08-29 09:51:45 +0200

    comments

--- readJoystick.ino
+++ readJoystick.ino
@@ -1,35 +1,44 @@
+// pin defining
 const byte x = A0;
 const byte y = A1;
 const int sw = 2;
 
+// x and y axis offsets (makes the measured values 0 when you're not moving the joystick)
 const int xOffset = 505;
 const int yOffset = 501;
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  // pinmodes
   pinMode(13, OUTPUT);
   pinMode(x, INPUT);
   pinMode(y, INPUT);
   pinMode(sw, INPUT_PULLUP);
 
+  // serial startup at 9600 baud
   Serial.begin(9600);
 }
 
 void loop() {
   
+  // fixing inaccuracy of x values
   int xVal = analogRead(x) - xOffset;
   if (xVal >= -1 && xVal <= 1) xVal = 0;
 
+  // fixing inaccuracy of y values
   int yVal = analogRead(y) - yOffset;
   if (yVal >= -1 && yVal <= 1) yVal = 0;
 
+  // printing "SWITCH PRESSED" to serial when switch is pressed
   if (digitalRead(sw) == LOW) Serial.println("SWITCH PRESSED");
 
+  // printing measured values to serial
   Serial.print("x value: ");
   Serial.println(xVal);
 
   Serial.print("y value: ");
   Serial.println(yVal);
   
+  // 8ms delay
   delay(8);
 }
