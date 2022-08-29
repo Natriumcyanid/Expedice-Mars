1cb03af5768d60f24ddaffd8fd58498ec8053ec9
Natriumcyanid <106405050+Natriumcyanid@users.noreply.github.com>
2022-08-29 09:17:56 +0200

    Update Robotužka.ino
    Comments

--- Robotužka.ino
+++ Robotužka.ino
@@ -3,11 +3,13 @@ int servo1 = 1;
 int servo2 = 2;
 int servo3 = 3;
 
+// Create servo objects
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
 void setup() {
+  // Attach servo objects to Arduino pins
   servo1.attach(servo1);
   servo2.attach(servo2);
   servo3.attach(servo3);
