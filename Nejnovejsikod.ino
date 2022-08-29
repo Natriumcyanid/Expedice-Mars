43bf4a6262a66103c8fd0ef7f9f60613d0b3891f
Kuba Vejrazka <jakvejr@gmail.com>
2022-08-29 17:01:51 +0200

    fixed servo control

--- Nejnovejsikod.ino
+++ Nejnovejsikod.ino
@@ -2,12 +2,13 @@
 
 static float current[2] = {0.0, 0.0};
 
-
 const int pinServo1 = 9;
 const int pinServo2 = 10;
 const int pinServo3 = 11;
 const int armA = 10;
 const int armB = 10;
+
+float i = 0.1;
 
 const byte x = A0;
 const byte y = A1;
@@ -16,12 +17,8 @@ const int sw = 2;
 const int xOffset = 505;
 const int yOffset = 501;
 
-int dX = 0;
-int dY = 0;
-
 float cX = current[0];
 float cY = current[1];
-float oldAngle;
 
 
 // Create servo objects
@@ -51,36 +48,37 @@ void setup() {
 void loop()
 {
   int xVal = analogRead(x) - xOffset;
-  if (xVal >= -1 && xVal <= 1) xVal = 0;
+  if (xVal < -50) cX -= i;
+  if (xVal > 50) cX += i;
+
+  if (cX > 180) cX = 180;
+  if (cX < -180) cX = -180;
 
   int yVal = analogRead(y) - yOffset;
-  if (yVal >= -1 && yVal <= 1) yVal = 0;
+  if (yVal < -50) cY -= i;
+  if (yVal > 50) cY += i;
 
-  float newAngle = GetAngle(xVal, yVal);
-  servo1.write(GetAngle(xVal, yVal));
-  servo2.write((asin((armB/armA)*sin((acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)))))));
-  servo3.write(acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)));
-  delay(100);
-  Serial.println(xVal, yVal);
-  Serial.println((asin((armB/armA)*sin((acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)))))));
-  Serial.println(acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)));
-  float oldAngle = newAngle;
+  if (cY > 180) cY = 180;
+  if (cY < 0) cY = 0;
+
+
+  Serial.print(cX);
+  Serial.print(" ");
+  Serial.println(cY);
+  servo1.write(GetAngle(cX, cY));
+  //servo2.write((asin((armB/armA)*sin((acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)))))));
+  //servo3.write(acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)));
 
   delay(5);
 }
 
 float GetAngle(float x, float y) {
-  float a = atan(y / x);
+  float a = atan(y / x); 
   a *= (180.0/3.1415);
-  return a;
+  return a + 90;
 }
 
 float GetDistance(int x, int y) {
   float d = sqrt(pow(x, 2) + pow(y, 2));
   return d;
 }
-
-//void rotate() {
-//(GetAngle(cX, cY));
- //(asin((armB/armA)*sin((acos((pow(GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB))))));
-//GetDistance(xVal, yVal), 2)-pow(armA, 2)-pow(armB, 2))/(-2*armA*armB)));
