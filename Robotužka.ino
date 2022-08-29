4f3bebe0a6410ea723f91abfbbf7f199e0a11a1c
Natriumcyanid <106405050+Natriumcyanid@users.noreply.github.com>
2022-08-29 09:44:02 +0200

    Update Robotužka.ino

--- Robotužka.ino
+++ Robotužka.ino
@@ -1,7 +1,7 @@
 #include <Servo.h> 
-const int servo1 = 1;
-const int servo2 = 2;
-const int servo3 = 3;
+const int pinServo1 = 1;
+const int pinServo2 = 2;
+const int pinServo3 = 3;
 
 // Create servo objects
 Servo servo1; // The servo that rotates the whole arm
@@ -10,9 +10,9 @@ Servo servo3; // The second limb
 
 void setup() {
   // Attach servo objects to Arduino pins
-  servo1.attach(servo1);
-  servo2.attach(servo2);
-  servo3.attach(servo3);
+  servo1.attach(pinServo1);
+  servo2.attach(pinServo2);
+  servo3.attach(pinServo3);
 }
 
 void loop() {
